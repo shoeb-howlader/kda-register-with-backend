3b23e4de29f7bb5bc1519fbd69a68e662d14f394
shoeb-howlader <mdshoebhawlader@gmail.com>
2021-11-27 01:12:38 +0600

    backup-mode added

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 //const logger=require('./logger');
-var winston = require('winston');
-require('winston-daily-rotate-file');
-var {format} = require('winston');
+//var winston = require('winston');
+//require('winston-daily-rotate-file');
+//var {format} = require('winston');
 /**
  * Logger handles all logs in the application
  */
-const logger = winston.createLogger({
+/*const logger = winston.createLogger({
   format: format.combine(format.timestamp(), format.simple()),
   colorize: true,
   transports: [
@@ -38,7 +38,7 @@ const logger = winston.createLogger({
 /**
  * morganLogger logs all http request in a dedicated file and on console
  */
-const morganLogger = winston.createLogger({
+/*const morganLogger = winston.createLogger({
   format: format.combine(format.simple()),
   transports: [
     new winston.transports.File({
@@ -66,11 +66,11 @@ const logStream = {
    * A writable stream for winston logger.
    *
    * @param {any} message
-   */
+   *//*
   write(message) {
     morganLogger.info(message.toString());
   }
-};
+};*/
 
 module.exports = async function() {
   try {
@@ -85,11 +85,11 @@ module.exports = async function() {
     .then(()=>{console.log('connection succesful')})
     .catch(err=>console.log(err))
     //console.log(logger)
-    logger.info('Connected to the database.');
-    //console.log('Connected to the database.');
+    //logger.info('Connected to the database.');
+    console.log('Connected to the database.');
   } catch (error) {
-    logger.error('Could not connect to the database.', error);
-    //console.log('Could not connect to the database.', error);
+    //logger.error('Could not connect to the database.', error);
+    console.log('Could not connect to the database.', error);
   }
 };
 
